f9a667c21c5773b29496345232edb42b98224123
Shavin77 <87821000+Shavin77@users.noreply.github.com>
2021-09-06 11:08:32 +0530

    Update __init__.py

--- SpamX/modules/__init__.py
+++ SpamX/modules/__init__.py
@@ -8,7 +8,7 @@ MOD_HELP = {}
 
 
 def list_all_modules() -> list:
-    modules_directory = "DaisyX/modules"
+    modules_directory = "SpamX/modules"
 
     all_modules = []
     for module_name in os.listdir(modules_directory):
